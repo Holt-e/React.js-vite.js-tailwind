75ef35c0e604d392690f11c9613689e08da1f6ce
Adrian Holthe <adrian.holthe@aibel.com>
2023-11-02 22:33:40 +0100

    yes

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -6,7 +6,8 @@ export function HelloWorld() {
   return (
     <div className="flex flex-col">
       <button
-        className="m-2 p-2 bg-gray-300"
+        className="m-2 p-2 bg-green-600+
+        "
         onClick={() => {
           setCount(count + 1);
         }}
@@ -14,7 +15,7 @@ export function HelloWorld() {
         count + 1
       </button>
       <button
-        className="m-2 p-2 bg-gray-300"
+        className="m-2 p-2 bg-red-600"
         onClick={() => {
           setCount(count - 1);
         }}
